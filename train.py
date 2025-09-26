32ddda41387b9650b97106626646d7ff4d3b28da
aditya8950958 <adityachoudhary5291@gmail.com>
2025-09-26 13:00:52 +0530

    Update train.py

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ from torch.utils.data import Dataset, DataLoader
 from nltk_utils import bag_of_words, tokenize, stem
 from model import NeuralNet
 
-# Load intents data
+
 with open('intents.json', 'r') as f:
     intents = json.load(f)
 
@@ -17,23 +17,23 @@ all_words = []
 tags = []
 xy = []
 
-# Loop through each sentence in our intents patterns
+
 for intent in intents['intents']:
     tag = intent['tag']
-    # Add to tag list
+    
     tags.append(tag)
     for pattern in intent['patterns']:
-        # Tokenize each word in the sentence
+        
         w = tokenize(pattern)
-        # Add to our words list
+        
         all_words.extend(w)
-        # Add to xy pair (pattern_sentence, tag)
+       
         xy.append((w, tag))
 
-# Stem and lower each word
+
 ignore_words = ['?', '.', '!']
 all_words = [stem(w) for w in all_words if w not in ignore_words]
-# Remove duplicates and sort
+
 all_words = sorted(set(all_words))
 tags = sorted(set(tags))
 
@@ -41,14 +41,14 @@ print(f'{len(xy)} patterns')
 print(f'{len(tags)} tags:', tags)
 print(f'{len(all_words)} unique stemmed words:', all_words)
 
-# Create training data
+
 X_train = []
 y_train = []
 for (pattern_sentence, tag) in xy:
-    # X: bag of words for each pattern_sentence
+   
     bag = bag_of_words(pattern_sentence, all_words)
     X_train.append(bag)
-    # y: PyTorch CrossEntropyLoss needs only class labels, not one-hot
+    
     label = tags.index(tag)
     y_train.append(label)
 
@@ -64,7 +64,7 @@ hidden_size = 8
 output_size = len(tags)
 print(input_size, output_size)
 
-# Create Dataset class for DataLoader
+
 class ChatDataset(Dataset):
 
     def __init__(self):
@@ -72,11 +72,11 @@ class ChatDataset(Dataset):
         self.x_data = X_train
         self.y_data = y_train
 
-    # support indexing such that dataset[i] can be used to get i-th sample
+    
     def __getitem__(self, index):
         return self.x_data[index], self.y_data[index]
 
-    # we can call len(dataset) to return the size
+    
     def __len__(self):
         return self.n_samples
 
@@ -88,25 +88,24 @@ train_loader = DataLoader(dataset=dataset,
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-# Initialize the model
 model = NeuralNet(input_size, hidden_size, output_size).to(device)
 
-# Loss and optimizer
+
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
 # Train the model
 for epoch in range(num_epochs):
     for (words, labels) in train_loader:
-        words = words.to(device)  # Move words to the correct device (CPU or GPU)
-        words = words.float()  # Convert to float32
+        words = words.to(device)  
+        words = words.float()  
         labels = labels.to(dtype=torch.long).to(device)
 
-        # Forward pass
+        
         outputs = model(words)
         loss = criterion(outputs, labels)
         
-        # Backward and optimize
+       
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
@@ -116,7 +115,7 @@ for epoch in range(num_epochs):
 
 print(f'Final loss: {loss.item():.4f}')
 
-# Save the trained model data
+
 data = {
     "model_state": model.state_dict(),
     "input_size": input_size,
