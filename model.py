38e3740b441e14f87c9a3b27b178a414aac6801d
aditya8950958 <adityachoudhary5291@gmail.com>
2025-09-26 13:01:42 +0530

    Update model.py

--- model.py
+++ model.py
@@ -15,5 +15,5 @@ class NeuralNet(nn.Module):
         out = self.l2(out)
         out = self.relu(out)
         out = self.l3(out)
-        # No activation and no softmax at the end
+       
         return out
